0b5caae624830053a4c79e2936e459b1c7d055d7
Vladislav <v.suvorkin2017@gmail.com>
2025-08-23 17:40:19 +0300

    chore: init project and install core dependencies

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "src/components/"),
+      "@services": path.resolve(__dirname, "src/services/"),
+      "@utils": path.resolve(__dirname, "src/utils/"),
+      "@styles": path.resolve(__dirname, "src/styles/"),
+      "@constants": path.resolve(__dirname, "src/constants/"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            targets: [">1%", "not dead"],
+            presets: [["@babel/preset-env"]],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[name][hash][ext]",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "main[contenthash].css",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 9001,
+    hot: true,
+  },
+};
