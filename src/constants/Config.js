b06923d9e4eef1560160bc765a3167af87974f1c
Vladislav <v.suvorkin2017@gmail.com>
2025-08-25 15:42:51 +0300

    chore: add popularMovieIDs in Config.js

---
+++ src/constants/Config.js
@@ -0,0 +1,184 @@
+export const popularMovieIDs = [
+  'tt0111161', // The Shawshank Redemption
+  'tt0068646', // The Godfather
+  'tt0468569', // The Dark Knight
+  'tt0071562', // The Godfather Part II
+  'tt0050083', // 12 Angry Men
+  'tt0108052', // Schindler's List
+  'tt0167260', // The Lord of the Rings: The Return of the King
+  'tt0110912', // Pulp Fiction
+  'tt0120737', // The Lord of the Rings: The Fellowship of the Ring
+  'tt0060196', // The Good, the Bad and the Ugly
+  'tt0133093', // The Matrix
+  'tt0109830', // Forrest Gump
+  'tt1375666', // Inception
+  'tt0167261', // The Lord of the Rings: The Two Towers
+  'tt0080684', // Star Wars: Episode V - The Empire Strikes Back
+  'tt0137523', // Fight Club
+  'tt0073486', // One Flew Over the Cuckoo's Nest
+  'tt0099685', // Goodfellas
+  'tt0047478', // Seven Samurai
+  'tt0114369', // Se7en
+  'tt0317248', // City of God
+  'tt0076759', // Star Wars: Episode IV - A New Hope
+  'tt0038650', // It's a Wonderful Life
+  'tt0102926', // The Silence of the Lambs
+  'tt0435761', // Toy Story 3
+  'tt0120815', // Saving Private Ryan
+  'tt0816692', // Interstellar
+  'tt0245429', // Spirited Away
+  'tt0120689', // The Green Mile
+  'tt0054215', // Psycho
+  'tt0118799', // Life Is Beautiful
+  'tt0034583', // Casablanca
+  'tt0027977', // Modern Times
+  'tt0253474', // The Pianist
+  'tt0172495', // Gladiator
+  'tt0110413', // Léon: The Professional
+  'tt0114814', // The Usual Suspects
+  'tt0088763', // Back to the Future
+  'tt0103064', // Terminator 2: Judgment Day
+  'tt0078788', // Apocalypse Now
+  'tt0057012', // Dr. Strangelove
+  'tt0021749', // City Lights
+  'tt0095765', // Cinema Paradiso
+  'tt0209144', // Memento
+  'tt0082971', // Indiana Jones and the Raiders of the Lost Ark
+  'tt0047396', // Rear Window
+  'tt0078748', // Alien
+  'tt0012349', // The Kid
+  'tt0052357', // Vertigo
+  'tt0095327', // Grave of the Fireflies
+  'tt0032553', // The Great Dictator
+  'tt0056172', // Lawrence of Arabia
+  'tt0081505', // The Shining
+  'tt0045152', // Singin' in the Rain
+  'tt0091251', // Come and See
+  'tt0075314', // Taxi Driver
+  'tt1832382', // A Separation
+  'tt0040875', // Bicycle Thieves
+  'tt0017136', // Metropolis
+  'tt0044741', // Ikiru
+  'tt0053125', // North by Northwest
+  'tt0070735', // The Sting
+  'tt0053604', // The Apartment
+  'tt0110357', // The Lion King
+  'tt0211915', // Amélie
+  'tt0040522', // Ladri di biciclette
+  'tt0056592', // To Kill a Mockingbird
+  'tt0093058', // Full Metal Jacket
+  'tt0064116', // Once Upon a Time in the West
+  'tt0071853', // Monty Python and the Holy Grail
+  'tt0119698', // Princess Mononoke
+  'tt0047296', // On the Waterfront
+  'tt0105236', // Reservoir Dogs
+  'tt0082096', // Das Boot
+  'tt0033467', // Citizen Kane
+  'tt0055630', // Yojimbo
+  'tt0071315', // Chinatown
+  'tt0042876', // Rashomon
+  'tt0057115', // The Great Escape
+  'tt0095016', // Die Hard
+  'tt0042192', // All About Eve
+  'tt0075686', // Annie Hall
+  'tt0119217', // Good Will Hunting
+  'tt0046912', // Dial M for Murder
+  'tt0053291', // Some Like It Hot
+  'tt0097576', // Indiana Jones and the Last Crusade
+  'tt0119488', // L.A. Confidential
+  'tt0096283', // My Neighbor Totoro
+  'tt0087843', // Once Upon a Time in America
+  'tt0040897', // The Treasure of the Sierra Madre
+  'tt0113277', // Heat
+  'tt0056801', // 8½
+  'tt0036775', // Double Indemnity
+  'tt0079944', // Stalker
+  'tt0052618', // Ben-Hur
+  'tt0091251', // Idi i smotri
+  'tt0066921', // A Clockwork Orange
+  'tt0055031', // Judgment at Nuremberg
+  'tt0086190', // Star Wars: Episode VI - Return of the Jedi
+  'tt0112641', // Casino
+  'tt0090605', // Aliens
+  'tt0050783', // Nights of Cabiria
+  'tt0114709', // Toy Story
+  'tt0022100', // M
+  'tt0180093', // Requiem for a Dream
+  'tt0041959', // The Third Man
+  'tt0051201', // Witness for the Prosecution
+  'tt0073195', // Jaws
+  'tt0095016', // Die Hard
+  'tt0117951', // Trainspotting
+  'tt0091763', // Platoon
+  'tt0015864', // The Gold Rush
+  'tt0046438', // Tokyo Story
+  'tt0116282', // Fargo
+  'tt0049470', // The Seventh Seal
+  'tt0050976', // The 400 Blows
+  'tt0050986', // Wild Strawberries
+  'tt0031381', // Gone with the Wind
+  'tt0050212', // The Bridge on the River Kwai
+  'tt0093779', // The Princess Bride
+  'tt0075148', // Rocky
+  'tt0080684', // The Empire Strikes Back
+  'tt0118849', // Children of Heaven
+  'tt0046268', // The Wages of Fear
+  'tt0092005', // Stand by Me
+  'tt0050613', // Throne of Blood
+  'tt0074958', // Network
+  'tt0118715', // The Big Lebowski
+  'tt0090605', // Aliens
+  'tt0031679', // Mr. Smith Goes to Washington
+  'tt0107290', // Jurassic Park
+  'tt0042876', // Rashomon
+  'tt0057565', // High and Low
+  'tt0112471', // Before Sunrise
+  'tt0093058', // Full Metal Jacket
+  'tt0113247', // La Haine
+  'tt0077416', // The Deer Hunter
+  'tt0072684', // Barry Lyndon
+  'tt0118715', // The Big Lebowski
+  'tt0083987', // Gandhi
+  'tt0093779', // The Princess Bride
+  'tt0056592', // To Kill a Mockingbird
+  'tt0095016', // Die Hard
+  'tt0047296', // On the Waterfront
+  'tt0105695', // Unforgiven
+  'tt0116282', // Fargo
+  'tt0088763', // Back to the Future
+  'tt0075148', // Rocky
+  'tt0082971', // Raiders of the Lost Ark
+  'tt0107290', // Jurassic Park
+  'tt0073195', // Jaws
+  'tt0081505', // The Shining
+  'tt0095016', // Die Hard
+  'tt0113277', // Heat
+  'tt0102926', // The Silence of the Lambs
+  'tt0114814', // The Usual Suspects
+  'tt0133093', // The Matrix
+  'tt0167260', // The Lord of the Rings: The Return of the King
+  'tt1375666', // Inception
+  'tt0137523', // Fight Club
+  'tt0120737', // The Lord of the Rings: The Fellowship of the Ring
+  'tt0110912', // Pulp Fiction
+  'tt0167261', // The Lord of the Rings: The Two Towers
+  'tt0816692', // Interstellar
+  'tt0076759', // Star Wars: Episode IV - A New Hope
+  'tt0086190', // Star Wars: Episode VI - Return of the Jedi
+  'tt0080684', // Star Wars: Episode V - The Empire Strikes Back
+  'tt0109830', // Forrest Gump
+  'tt0120815', // Saving Private Ryan
+  'tt0114369', // Se7en
+  'tt0073486', // One Flew Over the Cuckoo's Nest
+  'tt0108052', // Schindler's List
+  'tt0317248', // City of God
+  'tt0118799', // Life Is Beautiful
+  'tt0038650', // It's a Wonderful Life
+  'tt0047478', // Seven Samurai
+  'tt0054215', // Psycho
+  'tt0060196', // The Good, the Bad and the Ugly
+  'tt0071562', // The Godfather Part II
+  'tt0068646', // The Godfather
+  'tt0111161', // The Shawshank Redemption
+  'tt0468569', // The Dark Knight
+];
